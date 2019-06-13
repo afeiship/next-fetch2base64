138c2b846011e0c29d520a7e84ac98b0acfa21cd
afeiship <1290657123@qq.com>
2019-06-13 17:17:29 +0800

    fix: update fetch2base64

--- dist/next-fetch2base64.js
+++ dist/next-fetch2base64.js
@@ -9,7 +9,14 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var nxBlob2base64 = nx.blob2base64 || require('next-blob2base64');
-  var DEFAULT_OPTIONS = { method: 'GET', data: null };
+  var DEFAULT_OPTIONS = {
+    method: 'GET',
+    data: null,
+    callback: function(inBlob) {
+      var size = inBlob.size;
+      return inBlob.slice(0, size, 'image/png');
+    }
+  };
 
   var NxFetch2base64 = nx.declare('nx.Fetch2base64', {
     statics: {
@@ -22,7 +29,7 @@
 
         return new Promise(function(resolve, reject) {
           xhr.onload = function() {
-            nxBlob2base64(this.response).then(function(res) {
+            nxBlob2base64(this.response, { callback: options.callback }).then(function(res) {
               resolve(res);
             });
           };
